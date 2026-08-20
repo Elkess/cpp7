87ee8e9da8da850813e16057e288a6fd33d994dc
Elkess <mohamedamineel123@gmail>
2026-08-20 08:02:03 +0100

    ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,18 +1,51 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <stdexcept>
+#include <cstddef>
+
 template <class T>
 class Array
 {
 	private:
-		
+		T* array;
+		unsigned int len;
 	public:
-		Array();
-		Array(const Array& obj);
-		Array operator=(const Array& obj);
-		~Array();
-		size_t	size();
-};
+		Array(){
+			array = NULL;
+			len = 0;
+		}
+		Array(unsigned int n){
+			array = new T[n];
+			len = n;
+			for (size_t i = 0; i < len;i++)
+				array[i] = T();
+		}
+		Array(const Array& obj){
+			*this = obj;
+		}
+		Array &operator=(const Array& obj){
+			if (this == &obj)
+				return *this;
+			delete[] array;
+			len = obj.len;
+			array = new T[len];
+			for (size_t i = 0; i < len;i++)
+				array[i] = obj.array[i];
+			return *this;
+		}
+		T &operator[](unsigned int i){
+			if (i >= len)
+				throw std::out_of_range("Index out of bounds");
+			return array[i];
+		}
+		~Array(){
+			delete[] array;
+		}
 
+		unsigned int	size() const {
+			return len;
+		}
+};
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,1 +1,24 @@
-#include
+#include "Array.hpp"
+#include <iostream>
+
+int main()
+{
+	Array<int> a;
+	Array<int> b(10);
+
+	for (size_t i = 0; i < b.size(); i++)
+		std::cout << b[i] << " ";
+	std::cout << std::endl;
+
+	for (size_t i = 0; i < b.size(); i++)
+		b[i] = i;
+
+	for (size_t i = 0; i < b.size(); i++)
+		std::cout << b[i] << " ";
+	std::cout << std::endl;
+
+	a = b;
+	for (size_t i = 0; i < a.size(); i++)
+		std::cout << a[i] << " ";
+	std::cout << std::endl;
+}
